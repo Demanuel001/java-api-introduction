ada48bb87aada987175a57c7eabefcdc9577365b
Demanuel001 <daniloemanuel360@gmail.com>
2022-12-29 15:15:40 -0300

    Cria uma api basica

---
+++ README.md
@@ -0,0 +1,9 @@
+# API JAVA 
+## _API básica criada com java, versão 17_
+
+O projeto foi desenvolvido para fins acadêmicos.
+
+- Utilização do banco H2
+- Spring Boot
+- Spring Data JPA
+
---
+++ src/test/java/com/crmapi/crm/CrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmapi.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
